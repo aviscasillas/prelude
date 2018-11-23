7ad2105356731e9a97f4284a330913cc5a3e848a
Albert Viscasillas <aviscasillas@gmail.com>
2018-11-23 09:55:31 +0100

    Fix ensime config

--- personal/ensime.el
+++ personal/ensime.el
@@ -1,27 +1,19 @@
-;;; prelude-ensime.el --- Sbt mode intergration
-
-;;; Commentary:
-;;
-;;; Code:
 (prelude-require-package 'sbt-mode)
 (prelude-require-package 'ensime)
 (prelude-require-package 'scala-mode)
 
 (require 'scala-mode)
 (require 'sbt-mode)
+
 (defun play-start ()
   "Start play console."
   (interactive)
   (let ((sbt:program-name "play")) ; do a 'let' binding for the variable
     (sbt-start)))
 
-(require 'scala-mode)
-(require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 (setq scala-indent:use-javadoc-style t)
 (eval-after-load "flyspell"
   '(defun flyspell-prog-mode (&optional arg)))
 
-
-(provide 'prelude-ensime)
-;;; prelude-ensime.el ends here
+(setq ensime-startup-notification nil)
